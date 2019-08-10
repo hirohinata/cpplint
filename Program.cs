8bd96b8ef7da0e20fc4264bf02cc5dba8e776c9d
hirohinata <azuri_hirohinata@hotmail.co.jp>
2019-08-10 20:43:52 +0900

    add program

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+
+namespace CppLint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputStream = new AntlrInputStream(
+@"#include <iostream>
+
+int main()
+{
+    return 0;
+}
+");
+            var lexer = new CPPLINTLexer(inputStream);
+            var tokenStream = new CommonTokenStream(lexer);
+            var parser = new CPPLINTParser(tokenStream);
+            var tree = parser.translationunit();
+            var walker = new ParseTreeWalker();
+            walker.Walk(new Listener(), tree);
+        }
+    }
+}
