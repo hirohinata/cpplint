4b58cd8f60964cc1b18a098649d8ac09df84f265
hirohinata <azuri_hirohinata@hotmail.co.jp>
2019-08-10 20:42:47 +0900

    add listener

---
+++ Listener.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Antlr4.Runtime.Misc;
+
+namespace CppLint
+{
+    public class Listener : CPPLINTBaseListener
+    {
+        public Listener()
+        {
+        }
+
+        public override void EnterSelectionstatement([NotNull] CPPLINTParser.SelectionstatementContext context)
+        {
+        }
+
+        public override void ExitSelectionstatement([NotNull] CPPLINTParser.SelectionstatementContext context)
+        {
+        }
+    }
+}
